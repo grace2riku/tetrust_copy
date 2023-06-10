06e18c8fbe2b7a496727ff6a05a03ca5ce07fe18
Koji Abe <gracehime@gmail.com>
2023-06-10 21:19:30 +0900

    【Chapter 05 シンプルな落ちものパズルゲーム -> 6. ブロックを移動させる -> キー入力を判定して、ブロックを左右に移動】で左、下、右キーでブロックが移動することを確認した。

--- ch5/tetrust/src/main.rs
+++ ch5/tetrust/src/main.rs
@@ -78,10 +78,10 @@ struct Position {
 }
 
 // ブロックがフィールドに衝突する場合は'true'を返す
-fn is_collosion(field: &Field, pos: &Position, block: BlockKind) -> bool {
+fn is_collision(field: &Field, pos: &Position, block: BlockKind) -> bool {
     for y in 0..4 {
         for x in 0..4 {
-            if field[y+pos.y+1][x+pos.x] & BLOCKS[block as usize][y][x] == 1 {
+            if field[y+pos.y][x+pos.x] & BLOCKS[block as usize][y][x] == 1 {
                 return true;
             }
         }
@@ -123,10 +123,14 @@ fn main() {
     loop {
         // 描画用フィールドの生成
         let mut field_buf = field;
-        // 当り判定
-        if !is_collosion(&field, &pos, BlockKind::I) {
-            // posのy座標を更新
-            pos.y += 1;
+        // 自然落下
+        let new_pos = Position {
+            x: pos.x,
+            y: pos.y + 1,
+        };
+        if !is_collision(&field, &new_pos, BlockKind::I) {
+            // posの座標を更新
+            pos = new_pos;
         }
         // 描画用フィールドにブロックの情報を書き込む
         for y in 0..4 {
@@ -151,8 +155,38 @@ fn main() {
         }
         // 1秒間スリープする
         thread::sleep(time::Duration::from_millis(1000));
-        // 'q'キーでループを抜ける
+        // キー入力待ち
         match g.getch() {
+            Ok(Key::Left) => {
+                let new_pos = Position {
+                    x: pos.x - 1,
+                    y: pos.y,
+                };
+                if !is_collision(&field, &new_pos, BlockKind::I) {
+                    // posの座標を更新
+                    pos = new_pos;
+                }
+            }
+            Ok(Key::Down) => {
+                let new_pos = Position {
+                    x: pos.x,
+                    y: pos.y + 1,
+                };
+                if !is_collision(&field, &new_pos, BlockKind::I) {
+                    // posの座標を更新
+                    pos = new_pos;
+                }
+            }
+            Ok(Key::Right) => {
+                let new_pos = Position {
+                    x: pos.x + 1,
+                    y: pos.y,
+                };
+                if !is_collision(&field, &new_pos, BlockKind::I) {
+                    // posの座標を更新
+                    pos = new_pos;
+                }
+            }
             Ok(Key::Char('q')) => break,
             _ => (),   // 何もしない
         }
